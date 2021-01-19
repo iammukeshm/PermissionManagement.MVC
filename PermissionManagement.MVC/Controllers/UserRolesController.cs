b77ecc8a2196adcdf7ede0bf0f1941a6315ce652
Mukesh Murugan <iammukeshm@gmail.com>
2021-01-19 20:39:21 +0530

    added user role management

---
+++ PermissionManagement.MVC/Controllers/UserRolesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PermissionManagement.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionManagement.MVC.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index(string userId)
+        {
+            var viewModel = new List<UserRolesViewModel>();
+            var user = await _userManager.FindByIdAsync(userId);
+            foreach (var role in _roleManager.Roles)
+            {
+                var userRolesViewModel = new UserRolesViewModel
+                {
+                    RoleName = role.Name
+                };
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRolesViewModel.Selected = true;
+                }
+                else
+                {
+                    userRolesViewModel.Selected = false;
+                }
+                viewModel.Add(userRolesViewModel);
+            }
+            var model = new ManageUserRolesViewModel()
+            {
+                UserId = userId,
+                UserRoles = viewModel
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Update(string id, ManageUserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var roles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, roles);
+            result = await _userManager.AddToRolesAsync(user, model.UserRoles.Where(x => x.Selected).Select(y => y.RoleName));
+            var currentUser = await _userManager.GetUserAsync(User);
+            await _signInManager.RefreshSignInAsync(currentUser);
+            await Seeds.DefaultUsers.SeedSuperAdminAsync(_userManager, _roleManager);
+            return RedirectToAction("Index", new { userId = id });
+        }
+    }
+}
---
+++ PermissionManagement.MVC/Models/UserRolesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionManagement.MVC.Models
+{
+    public class ManageUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public IList<UserRolesViewModel> UserRoles { get; set; }
+    }
+
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
